81265391d0e02244a189ceab584279d08da827a4
Dmitry Sidorov <sidr@sidora.net>
2026-02-20 23:08:08 +0800

    init vite

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.jsx'),
+      name: 'SurveyWidget',
+      fileName: (format) => `survey-widget.${format}.js`,
+    },
+    rollupOptions: {
+      // Внешние зависимости, если React уже есть на сайте,
+      // или убираем external, если хотим все в одном файле (KISS)
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+      },
+    },
+  },
+});
